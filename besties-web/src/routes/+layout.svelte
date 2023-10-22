dae9777d1c98fdc4f4464f692029b1041e5af8c8
RosePetals01 <pankhurigoyalg@gmail.com>
2023-10-22 16:37:40 -0500

    Loading screen <3

---
+++ besties-web/src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script> 
+	import {onMount} from "svelte"
+
+
+	let isPageLoaded = false;
+
+	onMount(() =>{
+		isPageLoaded = true;
+	})
+
+	
+</script>
+
+{#if !isPageLoaded}
+	<div class = "loader">
+		<img src = "Besties_logo_2.png" alt  = "" />
+	</div>
+{/if}
+<slot />
+
+<style>
+	.loader{
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		display: grid;
+		place-items: center;
+		background-color: pink;
+		z-index: 9999;
+	}
+</style>
+
