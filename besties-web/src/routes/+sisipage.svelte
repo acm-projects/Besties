7cba8f1d20f61e98fe4f618015ba0e3a060d122c
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2023-11-10 23:59:43 -0600

    pre-game

---
+++ besties-web/src/routes/+sisipage.svelte
@@ -0,0 +1,234 @@
+</style>
+/* Pre-Game Screen */
+
+position: relative;
+width: 1440px;
+height: 1024px;
+
+background: #DF7A97;
+
+
+/* Rectangle 16 */
+
+position: absolute;
+width: 1440px;
+height: 360px;
+left: 0px;
+top: 0px;
+
+background: #F8ABC1;
+
+
+/* Pre-Game Time! */
+
+position: absolute;
+width: 995px;
+height: 174px;
+left: 222px;
+top: 46px;
+
+font-family: 'RocknRoll One';
+font-style: normal;
+font-weight: 400;
+font-size: 120px;
+line-height: 174px;
+/* identical to box height */
+
+color: #8B1D3E;
+
+border: 1px solid #8B1D3E;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+
+/* Answer these questions to determine your besties for the game! */
+
+position: absolute;
+width: 1200px;
+height: 58px;
+left: 120px;
+top: 268px;
+
+font-family: 'Average Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 45px;
+line-height: 58px;
+text-align: center;
+
+color: #8B1D3E;
+
+
+
+/* Questions */
+
+position: absolute;
+width: 992px;
+height: 508px;
+left: 222px;
+top: 441px;
+
+
+
+/* Q1 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 222px;
+top: 441px;
+
+
+
+/* Rectangle 10 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 222px;
+top: 441px;
+
+background: #FCCCDA;
+box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+
+
+/* 1: Question? */
+
+position: absolute;
+width: 176px;
+height: 45px;
+left: 332px;
+top: 525px;
+
+font-family: 'Average Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 45px;
+
+color: #8B1D3E;
+
+
+
+/* Q2 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 818px;
+top: 441px;
+
+
+
+/* Rectangle 11 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 818px;
+top: 441px;
+
+background: #FCCCDA;
+box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+
+
+/* 2: Question? */
+
+position: absolute;
+width: 179px;
+height: 45px;
+left: 926px;
+top: 525px;
+
+font-family: 'Average Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 45px;
+
+color: #8B1D3E;
+
+
+
+/* Q3 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 222px;
+top: 735px;
+
+
+
+/* Rectangle 12 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 222px;
+top: 735px;
+
+background: #FCCCDA;
+box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+
+
+/* 3: Question? */
+
+position: absolute;
+width: 179px;
+height: 45px;
+left: 330px;
+top: 815px;
+
+font-family: 'Average Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 45px;
+
+color: #8B1D3E;
+
+
+
+/* Q4 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 818px;
+top: 735px;
+
+
+
+/* Rectangle 13 */
+
+position: absolute;
+width: 396px;
+height: 214px;
+left: 818px;
+top: 735px;
+
+background: #FCCCDA;
+box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+
+
+/* 4: Question? */
+
+position: absolute;
+width: 178px;
+height: 45px;
+left: 927px;
+top: 815px;
+
+font-family: 'Average Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 35px;
+line-height: 45px;
+
+color: #8B1D3E;
+
+</style>
