3cf6f973a2b8a53f658b0e7586f6afb57a6ad2c8
Varshni <varshnik@gmail.com>
2023-11-19 19:02:41 -0600

    updated accent colors

--- besties-web/tailwind.config.js
+++ besties-web/tailwind.config.js
@@ -17,7 +17,7 @@ export default {
       mytheme: {
         "primary": "#E974A7",
         "secondary": "#8B1D3E",
-        "accent": "#8C448C",
+        "accent": "#7A1E71",
         "neutral": "#3d4451",
         "base-100": "#FFF5FA",
         "info": "#ECB8C7"
