3d0d9637c0b046617d75eca63cc1630688120741
Varshni <varshnik@gmail.com>
2023-11-24 18:41:29 -0600

    updated 5 questions navigation

--- besties-mobile/src/routes/Question/+page.svelte
+++ besties-mobile/src/routes/Question/+page.svelte
@@ -1,116 +1,179 @@
 <script>
+    import { tweened } from 'svelte/motion';
+
+    // -- Q&A DATA --
     const questions = [
 		{ 
-            "question": "What's the difference between an int and a double?",
-            "answers":["Numbers with a decimal", "idk", "The word is longer", "Nothing they are the same "]
+            "question": "What is one food that Ponyo loves to eat?",
+            "answers":["Ramen", "Ham", "Sandwiches", "Eggs"],
+            "pointsAvailable": 5,
+            "pointsEarned": 0
         },
         { 
-            "question": "What is 2+2?",
-            "answers":["7", "2", "4", "9"]
-        }
+            "question": "Which movie was the last Studio Ghibli movie?",
+            "answers":["Ponyo", "When Marnie was there", "The Boy and the Heron", "The Secret World of Arriety"],
+            "pointsAvailable": 25,
+            "pointsEarned": 0
+        },
+        { 
+            "question": "What happens at the beginning of The Grave of the Fireflies?",
+            "answers":["Seita is found dead at the station", "Seita and Setsuko are seen living in another country", "Seita, Setsuko, and their group are given aid", "Seita and Setsuko reunite with their mom"],
+            "pointsAvailable": 15,
+            "pointsEarned": 25
+        },
+        { 
+            "question": "What type of spirit is Haku in Spirited Away?",
+            "answers":["A wind spirit", "A water spirit", "A river spirit", "A mountain spirit"],
+            "pointsAvailable": 10,
+            "pointsEarned": 20
+        },
+        { 
+            "question": "What did Sophie make in Howl’s Moving Castle?",
+            "answers":["Shawls", "Hats", "Dresses", "Accessories"],
+            "pointsAvailable": 10,
+            "pointsEarned": 40
+        },
+
 	];
 
+
+    // -- TEAM OPTIONS --
     const teams = [
 		"Besties",
         "Wasted",
         "Michelin Archives"
 	];
-    
-    let points = 0;
 
-    function updatePoints() {
-		// console.log("makeFalse()");
-		points++;
-	}
+    // -- CURRENT QUESTION --
+    let qNum = 0;
+    function updateQNum(){
+        qNum++;
+        console.log("clickednext " + qNum + "  original seconds- " + seconds);
+        seconds = 20;
+        countdown();
+        
+        selectedColorBool = false;
+        selectedColorBool2 = false;
+        selectedColorBool3 = false;
+        selectedColorBool4 = false;
+        powerupColorBool = false;
+    }
+
+
+
+    // -- TIMER COUNTDOWN --
+    let original = 20; // total num of seconds
+	let timer = tweened(original)
+    setInterval(() => {
+            if ($timer > 0) $timer--;
+    }, 1000);
+
+    function countdown(){
+        // let original = 10;
+        timer = tweened(original)
+        // dont need to modify code below
+        setInterval(() => {
+            if ($timer > 0) $timer--;
+        }, 1000);
+    }
+    $: minutes = Math.floor($timer / 60);
+    $: seconds = Math.floor($timer - minutes * 60)
+
+
+    // -- ANSWERED BUTTON COLOR --
+    let selectedColorBool = false;
+    let selectedColorBool2 = false;
+    let selectedColorBool3 = false;
+    let selectedColorBool4 = false;
+
+    function testFunc(){
+        console.log("button 1 clicked ");
+        selectedColorBool = true;
+    }
+    function testFunc2(){
+        console.log("button 2 clicked ");
+        selectedColorBool2 = true;
+    }
+    function testFunc3(){
+        console.log("button 3 clicked ");
+        selectedColorBool3 = true;
+    }
+    function testFunc4(){
+        console.log("button 4 clicked ");
+        selectedColorBool4 = true;
+    }
+
+    // -- GIRLBOSS POWERUP --
+    let powerupColorBool = false;
+    function powerupFunc(){
+        powerupColorBool = true;
+    }
     
 </script>
 
 
 <body class="h-screen w-screen">
 
-    
-    <!-- <a href="/">Home</a> -->
-    
-    <!-- <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Access Powerups</button> -->
-    
-    <!-- <aCard class="card bg-base-300 w-screen">
-        <div class="card-body">
-            <p class="text-center">{questions[0].answers[0]}</p>        
-        </div>
-    </aCard> -->
-
-    <!-- place-content-center -->
-
     <topHeader class="grid w-screen rounded-b-2xl bg-info space-y-1">
         <span class="flex">
             <h1 class="text-secondary text-m font-bold">Team {teams[0]}</h1>
-            <h1 class="text-secondary text-m font-bold topHeader-points">{points} points</h1>
+            <h1 class="text-secondary text-m font-bold topHeader-points">{questions[qNum].pointsEarned} points</h1>
         </span>
 
     </topHeader>
 
     <!-- Question Card -->
     <qCard>
-        <div class="card bg-base-100 shadow-xl">
-            
+        <button onclick="my_modal_3.showModal()">
+            <div class="card bg-base-100 shadow-xl">
             <div class="card-body">
                 <!-- <p>12/30</p> -->
                 <span class="flex">
-                    <p>12/30</p>
-                    <p class="numPoints font-bold">5 points</p>
-                    
+                    <p>{qNum + 1}/5</p>
+                    <p class="numPoints font-bold">{questions[qNum].pointsAvailable} points</p>
                 </span>
-                <h2 class="card-title text-center">{questions[0].question}</h2>        
+                <h2 class="card-title text-center">{questions[qNum].question}</h2>        
             </div>
         </div>
+        </button>
+        
     </qCard>
 
     <!-- Radial Timer Countdown -->
     <timer>
-            <div class="radial-progress text-primary bg-white" style="--value:80; --size:4rem;">8</div>
+            <div class="radial-progress text-primary bg-white" style="--value:{seconds}*100/20; --size:4rem;">{seconds}</div>
     </timer>
 
-    <!-- <p class="text-center">{questions[0].answers[3]}</p> -->
 
     <!-- Answer Cards -->
     <answers class="flex flex-col space-y-5 ...">
-        <button class="answer-button">
-            <span> {questions[0].answers[0]} </span>
+        <button class="answer-button" class:selectedColor={selectedColorBool} on:click={testFunc}>
+            <span>{questions[qNum].answers[0]}</span>
         </button>
-        <button class="answer-button">
-            <span> {questions[0].answers[1]} </span>
+        <button class="answer-button" class:selectedColor2={selectedColorBool2} on:click={testFunc2}>
+            <span>{questions[qNum].answers[1]}</span>
         </button>
-        <button class="answer-button">
-            <span> {questions[0].answers[2]} </span>
+        <button class="answer-button" class:selectedColor3={selectedColorBool3} on:click={testFunc3}>
+            <span>{questions[qNum].answers[2]}</span>
         </button>
-        <button class="answer-button">
-            <span> {questions[0].answers[3]} </span>
+        <button class="answer-button" class:selectedColor4={selectedColorBool4} on:click={testFunc4}>
+            <span>{questions[qNum].answers[3]}</span>
         </button>
     </answers>
-    <!-- <answers class="flex flex-col space-y-4 ...">
-        <aCard class="card bg-base-300">
-            <div class="card-body">
-                <p class="text-center">{questions[0].answers[0]}</p>        
-            </div>
-        </aCard>
-        <aCard class="card bg-base-300">
-            <div class="card-body">
-                <p class="text-center">{questions[0].answers[1]}</p>        
-            </div>
-        </aCard>
-        <aCard class="card bg-base-300">
-            <div class="card-body">
-                <p class="text-center">{questions[0].answers[2]}</p>        
-            </div>
-        </aCard>
-        <aCard class="card bg-base-300">
-            <div class="card-body">
-                <p class="text-center">{questions[0].answers[3]}</p>        
-            </div>
-        </aCard>
-        
-    </answers> -->
 
+    <!-- Freeze Attack -->
+    <dialog id="my_modal_3" class="modal">
+    <div class="modal-box">
+        <h3 class="font-bold text-4xl text-center">Oh No! 😱</h3>
+        <h3 class="py-3 font-bold text-4xl text-center">You have been frozen by</h3>
+        <h3 class="py-3 font-bold text-4xl text-secondary text-center">Team Sisi!</h3>
+        <h3 class="py-5 font-bold text-2xl text-center">You won't be able to answer this question!</h3>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+    </dialog>   
+    
 
     <!-- Powerup Stuff -->
         
@@ -171,20 +234,13 @@
                         </select>
                     </span>
                 </button>
-                <button class="team-button">
+                <button class="team-button" class:powerupColor={powerupColorBool} on:click={powerupFunc}>
                     <span>
                         <span class="flex">
                             <h3 class="font-bold text-lg text-left ">Girlboss</h3>
                             <h3 class="font-bold text-lg text-left powerTwo">20 points</h3>
                         </span>
                         <p class="py-2 text-left">Earn double points for the next 30 seconds! 😇</p> 
-                    </span>
-                    <span>
-                        <select class="select select-bordered w-25 max-w-xs">
-                            <option disabled selected>Teams</option>
-                            <option>Han Solo</option>
-                            <option>Greedo</option>
-                        </select>
                     </span>
                 </button>
             </div>
@@ -197,38 +253,9 @@
     </powerupButton>
 
     <!-- Next Button -->
-    <button class="next-button font-bold">
+    <button on:click={updateQNum} class="next-button font-bold">
         <span> NEXT </span>
     </button>
-
-    
-
-    
-
-
-        
-        
- 
-    
-    
-    
-    <!-- <div>
-        <aCard class="card bg-base-300">
-            <div class="card-body">
-                <p class="text-center">{questions[0].answers[0]}</p>        
-            </div>
-        </aCard>
-    </div> -->
-
-    <!-- <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-    </div> -->
 
 </body>
 
@@ -341,6 +368,16 @@
         position:absolute; */
     }
 
+    .answer-buttonY:hover {
+        background-color: #04AA6D;
+        color: white;
+    }
+
+    .answer-buttonN:hover {
+        background-color: #f44336;
+        color: white;
+    }
+
     topHeader{
         height:50px;
         padding-top: 12px;
@@ -374,8 +411,28 @@
     }
 
     .numPoints{
-        padding-left: 192px;
+        padding-left: 190px;
         color:#96003A;
+    }
+
+    .selectedColor{
+        background-color: #04AA6D;
+    }
+
+    .selectedColor2{
+        background-color: #04AA6D;
+    }
+
+    .selectedColor3{
+        background-color: #04AA6D;
+    }
+
+    .selectedColor4{
+        background-color: #D1678E;
+    }
+
+    .powerupColor{
+        background-color: #D1678E;
     }
 
 
