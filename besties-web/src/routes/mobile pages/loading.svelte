86f917bb586fe8da1570165a87c27b77560371d9
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2023-11-25 16:50:44 -0600

    big fix

--- besties-web/src/routes/mobile pages/loading.svelte
+++ besties-web/src/routes/mobile pages/loading.svelte
@@ -1,71 +1,88 @@
 <script lang="ts">
-    // If you have any TypeScript variables or functions, they can be added here.
+    // TypeScript code
+    let loadingProgress = 30; // Example variable to show loading progress
   </script>
   
   <style>
-    .container {
+    .loading-container {
       position: relative;
-      width: 390px;
-      height: 844px;
+      width: 375px;
+      height: 812px;
       background: #FDC1D2;
     }
   
-    /* Assuming the vector is an SVG or image */
-    .vector {
+    .shadow {
       position: absolute;
-      width: 304px;
-      height: 256px;
-      left: 250px;
-      top: 547.06px;
-      background: #F3B2CA;
-      transform: rotate(-85.49deg);
+      width: 292px;
+      height: 23px;
+      left: 42px;
+      top: 413px;
+      background: radial-gradient(100.64% 213.71% at -5.14% 100%, #224BDA 0%, #5949DB 29.69%, #AD33B8 54.17%, #C42EA3 77.08%, #E93AB8 100%);
+      filter: blur(25px);
+      border-radius: 20px;
+    }
+  
+    .progress-bar {
+      position: absolute;
+      width: 301px;
+      height: 29px;
+      left: 42px;
+      top: 399px;
+      background: #FFFFFF;
+      border-radius: 20px;
+    }
+  
+    .progress {
+      position: absolute;
+      width: 30px; /* This width would be dynamic based on the loading progress */
+      height: 21px;
+      left: 50px;
+      top: 403px;
+      background: linear-gradient(60.14deg, #8B1D3E 5.72%, #E974A7 46.13%, #FFA9D7 76.28%);
+      border-radius: 20px;
+    }
+  
+    .ellipse-large {
+      position: absolute;
+      width: 103px;
+      height: 103px;
+      left: -192px;
+      top: 361px;
+      background: #FFFFFF;
+      mix-blend-mode: overlay;
+      filter: blur(25px);
+    }
+  
+    .ellipse-small {
+      position: absolute;
+      width: 60px;
+      height: 60px;
+      left: -18px;
+      top: 384px;
+      background: #FFFFFF;
+      mix-blend-mode: overlay;
+      filter: blur(25px);
     }
   
     .logo {
       position: absolute;
       width: 324px;
       height: 120px;
-      left: 33px;
-      top: 302px;
-      /* Placeholder for your image */
-      background: url('/path/to/your/image.png');
+      left: 26px;
+      top: 266px;
+      background: url('image.png');
       filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     }
-  
-    .loading-bar-container {
-      position: absolute;
-      width: 339px;
-      height: 13.86px;
-      left: 25px;
-      top: 460px;
-      filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    }
-  
-    .bar-background {
-      width: 100%;
-      height: 100%;
-      background: #EED4E9;
-      border-radius: 20px;
-    }
-  
-    .bar-progress {
-      width: 58%; /* Assuming a progress of 58% */
-      height: 100%;
-      background: #8B1D3E;
-      border-radius: 20px;
-    }
-  
-    /* Additional vector styles omitted for brevity */
   </style>
   
-  <div class="container">
-    <div class="vector"></div>
-    <div class="logo"></div>
-    <div class="loading-bar-container">
-      <div class="bar-background">
-        <div class="bar-progress"></div>
-      </div>
+  <div class="loading-container">
+    <div class="shadow"></div>
+    <div class="progress-bar">
+      <div class="progress" style="width: {loadingProgress}px;"></div>
     </div>
-    <!-- Additional vectors omitted for brevity -->
+    <div class="ellipse-large"></div>
+    <div class="ellipse-small"></div>
+    <div class="logo"></div>
+    <!-- Additional markup for logos, etc. -->
   </div>
   
