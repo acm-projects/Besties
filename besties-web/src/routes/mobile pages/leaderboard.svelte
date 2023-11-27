37d93a7e9c5d68f4cbf6a5a153b50087644faecf
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2023-11-27 16:17:26 -0600

    i am about to end it all

--- besties-web/src/routes/mobile pages/leaderboard.svelte
+++ besties-web/src/routes/mobile pages/leaderboard.svelte
@@ -1,296 +1,352 @@
 <script lang="ts">
-  </script>
-  
-  <style>
-    /* Add your CSS styles here */
-    /* You can use Svelte's style tag for CSS-in-JS */
-  
-    .group12 {
-      position: absolute;
-      width: 414px;
-      height: 844px;
-    }
-  
-    .iphone {
-      position: absolute;
-      width: 414px;
-      height: 844px;
-      overflow-y: scroll;
-      background: #7A1E71;
-    }
-  
-    .rectangle1 {
-      position: absolute;
-      width: 430px;
-      height: 604px;
-      left: -8px;
-      top: 0px;
-      background: #E36C8F;
-      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
-      border-radius: 30px;
-    }
-  
-    .ellipse1 {
-      position: absolute;
-      width: 166px;
-      height: 156px;
-      left: 124px;
-      top: 169px;
-      background: #F8C359;
-      border: 3px solid #FFAA00;
-    }
-  
-    .crown {
-      position: absolute;
-      width: 90px;
-      height: 74px;
-      left: 161px;
-      top: 206px;
-    }
-  
-    .vector {
-      position: absolute;
-      left: 40.81%;
-      right: 41.29%;
-      top: 26.7%;
-      bottom: 66.82%;
-      background: #FFAA00;
-    }
-  
-    .ellipse4 {
-      position: absolute;
-      width: 21.18px;
-      height: 22.08px;
-      left: 195.41px;
-      top: 206px;
-      background: #FFAA00;
-    }
-  
-    .ellipse7 {
-      position: absolute;
-      width: 15.88px;
-      height: 16.56px;
-      left: 198.06px;
-      top: 247.39px;
-      background: #957916;
-    }
-  
-    .ellipse5 {
-      position: absolute;
-      width: 13.24px;
-      height: 13.8px;
-      left: 237.76px;
-      top: 228.07px;
-      background: #FFAA00;
-    }
-  
-    .ellipse6 {
-      position: absolute;
-      width: 13.24px;
-      height: 13.8px;
-      left: 161px;
-      top: 228.07px;
-      background: #FFAA00;
-    }
-  
-    .ellipse8 {
-      position: absolute;
-      width: 7.94px;
-      height: 8.28px;
-      left: 182.18px;
-      top: 255.67px;
-      background: #957916;
-    }
-  
-    .ellipse9 {
-      position: absolute;
-      width: 7.94px;
-      height: 8.28px;
-      left: 221.88px;
-      top: 255.67px;
-      background: #957916;
-    }
-  
-    .rectangle7 {
-      position: absolute;
-      width: 33.39px;
-      height: 33.39px;
-      left: 207.04px;
-      top: 298.28px;
-      background: #FFAA00;
-      border-radius: 5px;
-      transform: matrix(0.73, 0.68, -0.73, 0.68, 0, 0);
-    }
-  
-    .text1 {
-      position: absolute;
-      width: 8px;
-      height: 19px;
-      left: 203px;
-      top: 312px;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 16px;
-      line-height: 19px;
-      color: #FFFFFF;
-    }
-  
-    .congrats {
-      position: absolute;
-      left: 6.76%;
-      right: 7.49%;
-      top: 44.55%;
-      bottom: 51.66%;
-      font-family: 'Average Sans';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 25px;
-      line-height: 32px;
-      text-align: center;
-      color: #FFF1F5;
-    }
-  
-    .group10 {
-      position: absolute;
-      width: 364px;
-      height: 76px;
-      left: 28px;
-      top: 739px;
-    }
-  
-    .rectangle65 {
-      position: absolute;
-      width: 364px;
-      height: 76px;
-      left: 28px;
-      top: 739px;
-      background: #E36C8F;
-      border-radius: 10px;
-    }
-  
-    .text2 {
-      position: absolute;
-      width: 45px;
-      height: 25px;
-      left: 280px;
-      top: 764px;
-      font-family: 'Average Sans';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 19px;
-      line-height: 25px;
-      color: #FFFFFF;
-    }
-  
-    .team1 {
-      position: absolute;
-      width: 89px;
-      height: 39px;
-      left: 92px;
-      top: 758px;
-      font-family: 'Average Sans';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 30px;
-      line-height: 39px;
-      color: #FFFFFF;
-    }
-  
-    .group11 {
-      position: absolute;
-      width: 364px;
-      height: 76px;
-      left: 28px;
-      top: 645px;
-    }
-  
-    .rectangle66 {
-      position: absolute;
-      width: 364px;
-      height: 76px;
-      left: 28px;
-      top: 645px;
-      background: #E36C8F;
-      border-radius: 10px;
-    }
-  
-    .text3 {
-      position: absolute;
-      width: 45px;
-      height: 25px;
-      left: 280px;
-      top: 670px;
-      font-family: 'Average Sans';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 19px;
-      line-height: 25px;
-      color: #FFFFFF;
-    }
-  
-    .team2 {
-      position: absolute;
-      width: 85px;
-      height: 39px;
-      left: 92px;
-      top: 664px;
-      font-family: 'Average Sans';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 30px;
-      line-height: 39px;
-      color: #FFFFFF;
-    }
-  
-    .heart1 {
-      position: absolute;
-      width: 38px;
-      height: 34px;
-      left: 44px;
-      top: 761px;
-      background: url('purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png.png');
-      filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    }
-  
-    .heart2 {
-      position: absolute;
-      width: 38px;
-      height: 34px;
-      left: 44px;
-      top: 667px;
-      background: url('purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png.png');
-      filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    }
-  </style>
-  
-  <div class="group12">
+  // Import any necessary dependencies or styles here
+</script>
+
+<style>
+  /* Mask group */
+  .mask-group {
+    position: absolute;
+    width: 414px;
+    height: 844px;
+  }
+
+  /* Group 12 */
+  .group-12 {
+    position: absolute;
+    width: 414px;
+    height: 844px;
+  }
+
+  /* iPhone */
+  .iphone {
+    position: absolute;
+    width: 414px;
+    height: 844px;
+    overflow-y: scroll;
+    background: #7A1E71;
+  }
+
+  /* Rectangle 1 */
+  .rectangle-1 {
+    position: absolute;
+    width: 430px;
+    height: 604px;
+    left: -8px;
+    top: 0px;
+    background: #E36C8F;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 30px;
+  }
+
+  /* Ellipse 1 */
+  .ellipse-1 {
+    position: absolute;
+    width: 166px;
+    height: 156px;
+    left: 124px;
+    top: 169px;
+    background: #F8C359;
+    border: 3px solid #FFAA00;
+  }
+
+  /* Crown */
+  .crown {
+    position: absolute;
+    width: 90px;
+    height: 74px;
+    left: 161px;
+    top: 206px;
+  }
+
+  /* Vector */
+  .vector {
+    position: absolute;
+    left: 40.81%;
+    right: 41.29%;
+    top: 26.7%;
+    bottom: 66.82%;
+    background: #FFAA00;
+  }
+
+  /* Ellipse 4 */
+  .ellipse-4 {
+    position: absolute;
+    width: 21.18px;
+    height: 22.08px;
+    left: 195.41px;
+    top: 206px;
+    background: #FFAA00;
+  }
+
+  /* Ellipse 7 */
+  .ellipse-7 {
+    position: absolute;
+    width: 15.88px;
+    height: 16.56px;
+    left: 198.06px;
+    top: 247.39px;
+    background: #957916;
+  }
+
+  /* Ellipse 5 */
+  .ellipse-5 {
+    position: absolute;
+    width: 13.24px;
+    height: 13.8px;
+    left: 237.76px;
+    top: 228.07px;
+    background: #FFAA00;
+  }
+
+  /* Ellipse 6 */
+  .ellipse-6 {
+    position: absolute;
+    width: 13.24px;
+    height: 13.8px;
+    left: 161px;
+    top: 228.07px;
+    background: #FFAA00;
+  }
+
+  /* Ellipse 8 */
+  .ellipse-8 {
+    position: absolute;
+    width: 7.94px;
+    height: 8.28px;
+    left: 182.18px;
+    top: 255.67px;
+    background: #957916;
+  }
+
+  /* Ellipse 9 */
+  .ellipse-9 {
+    position: absolute;
+    width: 7.94px;
+    height: 8.28px;
+    left: 221.88px;
+    top: 255.67px;
+    background: #957916;
+  }
+
+  /* Rectangle 7 */
+  .rectangle-7 {
+    position: absolute;
+    width: 33.39px;
+    height: 33.39px;
+    left: 207.04px;
+    top: 298.28px;
+    background: #FFAA00;
+    border-radius: 5px;
+    transform: matrix(0.73, 0.68, -0.73, 0.68, 0, 0);
+  }
+
+  /* 1 */
+  .text-1 {
+    position: absolute;
+    width: 8px;
+    height: 19px;
+    left: 203px;
+    top: 312px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 19px;
+    color: #FFFFFF;
+  }
+
+  /* Congrats! You are in 1st Place */
+  .text-2 {
+    position: absolute;
+    left: 6.76%;
+    right: 7.49%;
+    top: 44.55%;
+    bottom: 51.66%;
+    font-family: 'Average Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 25px;
+    line-height: 32px;
+    text-align: center;
+    color: #FFF1F5;
+  }
+
+  /* Group 10 */
+  .group-10 {
+    position: absolute;
+    width: 364px;
+    height: 76px;
+    left: 28px;
+    top: 739px;
+  }
+
+  /* Rectangle 65 */
+  .rectangle-65-1 {
+    position: absolute;
+    width: 364px;
+    height: 76px;
+    left: 28px;
+    top: 739px;
+    background: #E36C8F;
+    border-radius: 10px;
+  }
+
+  /* 43pts */
+  .text-3 {
+    position: absolute;
+    width: 44px;
+    height: 25px;
+    left: 280px;
+    top: 764px;
+    font-family: 'Average Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 25px;
+    color: #FFFFFF;
+  }
+
+  /* Team 1 */
+  .text-4 {
+    position: absolute;
+    width: 89px;
+    height: 39px;
+    left: 92px;
+    top: 758px;
+    font-family: 'Average Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 30px;
+    line-height: 39px;
+    color: #FFFFFF;
+  }
+
+  /* Group 11 */
+  .group-11 {
+    position: absolute;
+    width: 364px;
+    height: 76px;
+    left: 28px;
+    top: 645px;
+  }
+
+  /* Rectangle 65 */
+  .rectangle-65-2 {
+    position: absolute;
+    width: 364px;
+    height: 76px;
+    left: 28px;
+    top: 645px;
+    background: #E36C8F;
+    border-radius: 10px;
+  }
+
+  /* 500pt */
+  .text-5 {
+    position: absolute;
+    width: 45px;
+    height: 25px;
+    left: 280px;
+    top: 670px;
+    font-family: 'Average Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 25px;
+    color: #FFFFFF;
+  }
+
+  /* Team 2 */
+  .text-6 {
+    position: absolute;
+    width: 85px;
+    height: 39px;
+    left: 92px;
+    top: 664px;
+    font-family: 'Average Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 30px;
+    line-height: 39px;
+    color: #FFFFFF;
+  }
+
+  /* purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png 1 */
+  .image-1 {
+    position: absolute;
+    width: 38px;
+    height: 34px;
+    left: 44px;
+    top: 761px;
+    background: url(purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png);
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+
+  /* purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png 2 */
+  .image-2 {
+    position: absolute;
+    width: 38px;
+    height: 34px;
+    left: 44px;
+    top: 667px;
+    background: url(purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png);
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+
+  /* Vector 1 */
+  .vector-1 {
+    position: absolute;
+    left: 84.3%;
+    right: 10.87%;
+    top: 80.33%;
+    bottom: 18.48%;
+    border: 4.16667px solid #0D782B;
+  }
+
+  /* Vector 2 */
+  .vector-2 {
+    position: absolute;
+    left: 84.3%;
+    right: 10.87%;
+    top: 92.06%;
+    bottom: 6.75%;
+    border: 4.16667px solid #691C17;
+  }
+</style>
+
+<div class="mask-group">
+  <div class="group-12">
     <div class="iphone">
-      <div class="rectangle1"></div>
-      <div class="ellipse1"></div>
+      <div class="rectangle-1"></div>
+      <div class="ellipse-1"></div>
       <div class="crown"></div>
       <div class="vector"></div>
-      <div class="ellipse4"></div>
-      <div class="ellipse7"></div>
-      <div class="ellipse5"></div>
-      <div class="ellipse6"></div>
-      <div class="ellipse8"></div>
-      <div class="ellipse9"></div>
-      <div class="rectangle7"></div>
-      <div class="text1">1</div>
-      <div class="congrats">Congrats! You are in 1st Place</div>
-    </div>
-    <div class="group10">
-      <div class="rectangle65"></div>
-      <div class="text2">500pt</div>
-      <div class="team1">Team 1</div>
-      <div class="heart1"></div>
-    </div>
-    <div class="group11">
-      <div class="rectangle66"></div>
-      <div class="text3">500pt</div>
-      <div class="team2">Team 2</div>
-      <div class="heart2"></div>
+      <div class="ellipse-4"></div>
+      <div class="ellipse-7"></div>
+      <div class="ellipse-5"></div>
+      <div class="ellipse-6"></div>
+      <div class="ellipse-8"></div>
+      <div class="ellipse-9"></div>
+      <div class="rectangle-7"></div>
+      <div class="text-1">1</div>
+      <div class="text-2">Congrats! You are in 1st Place</div>
     </div>
   </div>
-  
+</div>
+
+<div class="group-10">
+  <div class="rectangle-65-1"></div>
+  <div class="text-3">43pts</div>
+  <div class="text-4">Team 1</div>
+  <div class="image-1"></div>
+</div>
+
+<div class="group-11">
+  <div class="rectangle-65-2"></div>
+  <div class="text-5">500pt</div>
+  <div class="text-6">Team 2</div>
+  <div class="image-2"></div>
+</div>
+
+<div class="vector-1"></div>
+<div class="vector-2"></div>
