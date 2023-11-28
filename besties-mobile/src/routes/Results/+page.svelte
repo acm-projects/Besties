910fabd41a1000bb4e951143938a239e04718975
Varshni <varshnik@gmail.com>
2023-11-27 19:46:02 -0600

    new results screen

--- besties-mobile/src/routes/Results/+page.svelte
+++ besties-mobile/src/routes/Results/+page.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+  // Import any necessary dependencies or styles here
 </script>
 
 <style>
-  /* Add your CSS styles here */
-  /* You can use Svelte's style tag for CSS-in-JS */
-
-  .group12 {
+  /* Mask group */
+  .mask-group {
     position: absolute;
     width: 414px;
     height: 844px;
   }
 
+  /* Group 12 */
+  .group-12 {
+    position: absolute;
+    width: 414px;
+    height: 844px;
+  }
+
+  /* iPhone */
   .iphone {
     position: absolute;
     width: 414px;
@@ -19,7 +26,8 @@
     background: #7A1E71;
   }
 
-  .rectangle1 {
+  /* Rectangle 1 */
+  .rectangle-1 {
     position: absolute;
     width: 430px;
     height: 604px;
@@ -30,7 +38,8 @@
     border-radius: 30px;
   }
 
-  .ellipse1 {
+  /* Ellipse 1 */
+  .ellipse-1 {
     position: absolute;
     width: 166px;
     height: 156px;
@@ -40,6 +49,7 @@
     border: 3px solid #FFAA00;
   }
 
+  /* Crown */
   .crown {
     position: absolute;
     width: 90px;
@@ -48,6 +58,7 @@
     top: 206px;
   }
 
+  /* Vector */
   .vector {
     position: absolute;
     left: 40.81%;
@@ -57,7 +68,8 @@
     background: #FFAA00;
   }
 
-  .ellipse4 {
+  /* Ellipse 4 */
+  .ellipse-4 {
     position: absolute;
     width: 21.18px;
     height: 22.08px;
@@ -66,7 +78,8 @@
     background: #FFAA00;
   }
 
-  .ellipse7 {
+  /* Ellipse 7 */
+  .ellipse-7 {
     position: absolute;
     width: 15.88px;
     height: 16.56px;
@@ -75,7 +88,8 @@
     background: #957916;
   }
 
-  .ellipse5 {
+  /* Ellipse 5 */
+  .ellipse-5 {
     position: absolute;
     width: 13.24px;
     height: 13.8px;
@@ -84,7 +98,8 @@
     background: #FFAA00;
   }
 
-  .ellipse6 {
+  /* Ellipse 6 */
+  .ellipse-6 {
     position: absolute;
     width: 13.24px;
     height: 13.8px;
@@ -93,7 +108,8 @@
     background: #FFAA00;
   }
 
-  .ellipse8 {
+  /* Ellipse 8 */
+  .ellipse-8 {
     position: absolute;
     width: 7.94px;
     height: 8.28px;
@@ -102,7 +118,8 @@
     background: #957916;
   }
 
-  .ellipse9 {
+  /* Ellipse 9 */
+  .ellipse-9 {
     position: absolute;
     width: 7.94px;
     height: 8.28px;
@@ -111,7 +128,8 @@
     background: #957916;
   }
 
-  .rectangle7 {
+  /* Rectangle 7 */
+  .rectangle-7 {
     position: absolute;
     width: 33.39px;
     height: 33.39px;
@@ -122,7 +140,8 @@
     transform: matrix(0.73, 0.68, -0.73, 0.68, 0, 0);
   }
 
-  .text1 {
+  /* 1 */
+  .text-1 {
     position: absolute;
     width: 8px;
     height: 19px;
@@ -136,7 +155,8 @@
     color: #FFFFFF;
   }
 
-  .congrats {
+  /* Congrats! You are in 1st Place */
+  .text-2 {
     position: absolute;
     left: 6.76%;
     right: 7.49%;
@@ -151,7 +171,8 @@
     color: #FFF1F5;
   }
 
-  .group10 {
+  /* Group 10 */
+  .group-10 {
     position: absolute;
     width: 364px;
     height: 76px;
@@ -159,7 +180,8 @@
     top: 739px;
   }
 
-  .rectangle65 {
+  /* Rectangle 65 */
+  .rectangle-65-1 {
     position: absolute;
     width: 364px;
     height: 76px;
@@ -169,9 +191,10 @@
     border-radius: 10px;
   }
 
-  .text2 {
+  /* 43pts */
+  .text-3 {
     position: absolute;
-    width: 45px;
+    width: 44px;
     height: 25px;
     left: 280px;
     top: 764px;
@@ -183,7 +206,8 @@
     color: #FFFFFF;
   }
 
-  .team1 {
+  /* Team 1 */
+  .text-4 {
     position: absolute;
     width: 89px;
     height: 39px;
@@ -197,7 +221,8 @@
     color: #FFFFFF;
   }
 
-  .group11 {
+  /* Group 11 */
+  .group-11 {
     position: absolute;
     width: 364px;
     height: 76px;
@@ -205,7 +230,8 @@
     top: 645px;
   }
 
-  .rectangle66 {
+  /* Rectangle 65 */
+  .rectangle-65-2 {
     position: absolute;
     width: 364px;
     height: 76px;
@@ -215,7 +241,8 @@
     border-radius: 10px;
   }
 
-  .text3 {
+  /* 500pt */
+  .text-5 {
     position: absolute;
     width: 45px;
     height: 25px;
@@ -229,7 +256,8 @@
     color: #FFFFFF;
   }
 
-  .team2 {
+  /* Team 2 */
+  .text-6 {
     position: absolute;
     width: 85px;
     height: 39px;
@@ -243,53 +271,82 @@
     color: #FFFFFF;
   }
 
-  .heart1 {
+  /* purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png 1 */
+  .image-1 {
     position: absolute;
     width: 38px;
     height: 34px;
     left: 44px;
     top: 761px;
-    background: url('purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png.png');
+    background: url(purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
 
-  .heart2 {
+  /* purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png 2 */
+  .image-2 {
     position: absolute;
     width: 38px;
     height: 34px;
     left: 44px;
     top: 667px;
-    background: url('purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png.png');
+    background: url(purple-heart-sign-isolated-on-transparent-background-valentines-day-icon-hand-drawn-heart-shape-world-heart-day-concept-love-icon-illustration-free-png);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+
+  /* Vector 1 */
+  .vector-1 {
+    position: absolute;
+    left: 84.3%;
+    right: 10.87%;
+    top: 80.33%;
+    bottom: 18.48%;
+    border: 4.16667px solid #0D782B;
+  }
+
+  /* Vector 2 */
+  .vector-2 {
+    position: absolute;
+    left: 84.3%;
+    right: 10.87%;
+    top: 92.06%;
+    bottom: 6.75%;
+    border: 4.16667px solid #691C17;
   }
 </style>
 
-<div class="group12">
-  <div class="iphone">
-    <div class="rectangle1"></div>
-    <div class="ellipse1"></div>
-    <div class="crown"></div>
-    <div class="vector"></div>
-    <div class="ellipse4"></div>
-    <div class="ellipse7"></div>
-    <div class="ellipse5"></div>
-    <div class="ellipse6"></div>
-    <div class="ellipse8"></div>
-    <div class="ellipse9"></div>
-    <div class="rectangle7"></div>
-    <div class="text1">1</div>
-    <div class="congrats">Congrats! You are in 1st Place</div>
-  </div>
-  <div class="group10">
-    <div class="rectangle65"></div>
-    <div class="text2">500pt</div>
-    <div class="team1">Team 1</div>
-    <div class="heart1"></div>
-  </div>
-  <div class="group11">
-    <div class="rectangle66"></div>
-    <div class="text3">500pt</div>
-    <div class="team2">Team 2</div>
-    <div class="heart2"></div>
+<div class="mask-group">
+  <div class="group-12">
+    <div class="iphone">
+      <div class="rectangle-1"></div>
+      <div class="ellipse-1"></div>
+      <div class="crown"></div>
+      <div class="vector"></div>
+      <div class="ellipse-4"></div>
+      <div class="ellipse-7"></div>
+      <div class="ellipse-5"></div>
+      <div class="ellipse-6"></div>
+      <div class="ellipse-8"></div>
+      <div class="ellipse-9"></div>
+      <div class="rectangle-7"></div>
+      <div class="text-1">1</div>
+      <div class="text-2">Congrats! You are in 1st Place</div>
+    </div>
   </div>
 </div>
+
+<div class="group-10">
+  <div class="rectangle-65-1"></div>
+  <div class="text-3">43pts</div>
+  <div class="text-4">Team 1</div>
+  <div class="image-1"></div>
+</div>
+
+<div class="group-11">
+  <div class="rectangle-65-2"></div>
+  <div class="text-5">500pt</div>
+  <div class="text-6">Team 2</div>
+  <div class="image-2"></div>
+</div>
+
+<div class="vector-1"></div>
+<div class="vector-2"></div>
